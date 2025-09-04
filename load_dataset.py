3d89881331df0876419d7abf841c3cab98559e2f
Giles Thomas <giles@giles.net>
2025-09-04 20:39:45 +0100

    QOL improvements

--- load_dataset.py
+++ load_dataset.py
@@ -3,7 +3,6 @@ import time
 
 import click
 import torch
-import torch.nn.functional as F
 
 from torch.utils.data import Dataset
 
@@ -13,15 +12,19 @@ class NextByteDataset(Dataset):
     def __init__(self, full_data, seq_length):
         super().__init__()
 
+        assert seq_length > 0, "Sequence length must be > 0"
         self.seq_length = seq_length
 
         self.num_sequences = (len(full_data) - 1) // self.seq_length
+        assert self.num_sequences > 0, "Not enough data for any sequences"
+
         self.data = full_data[:(self.num_sequences * self.seq_length) + 1]
 
         values_used = set(self.data)
+        self.id_to_byte = {ii: byte for (ii, byte) in enumerate(sorted(values_used))}
         self.byte_to_id = {
             byte: ii
-            for (ii, byte) in enumerate(sorted(values_used))
+            for (ii, byte) in self.id_to_byte.items()
         }
         self.data_as_ids = torch.tensor(
             [self.byte_to_id[b] for b in self.data],
