7c75adab7e4d5768797a1144bc68d81386f2b22a
Giles Thomas <giles@giles.net>
2025-09-04 19:04:49 +0100

    Moved load_data_test to the beginnings of a new module to create our dataset

--- load_dataset.py
+++ load_dataset.py
@@ -1,23 +1,27 @@
+import os
 import time
 
 import click
 import torch
 
 
-@click.command()
-@click.argument("filename")
-def main(filename):
-    start = time.time()
+def load_dataset(directory):
+    filename = os.path.join(directory, "input.txt")
     with open(filename, "rb") as f:
         data = f.read()
 
     characters = set(data)
     byte_to_id = {byte: ii for (ii, byte) in enumerate(sorted(characters))}
     data = torch.tensor([byte_to_id[byte] for byte in data], dtype=torch.long)
-    print(time.time() - start)
 
+
+@click.command()
+@click.argument("directory")
+def main(directory):
+    start = time.time()
+    load_dataset(directory)
+    print(time.time() - start)
 
 
 if __name__ == "__main__":
     main()
-
