8e7ff69a426c4649e2d2a7ee4f7f8fbf2911b324
Giles Thomas <giles@giles.net>
2025-09-17 23:34:58 +0100

    Moved the KarpathyLSTM to its own module

---
+++ karpathy_lstm.py
@@ -0,0 +1,39 @@
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class KarpathyLSTM(nn.Module):
+
+    def __init__(self, vocab_size, hidden_size, num_layers, dropout):
+        super().__init__()
+
+        self.vocab_size = vocab_size
+
+        self.lstm = nn.LSTM(
+            input_size=vocab_size,
+            hidden_size=hidden_size,
+            num_layers=num_layers,
+            dropout=dropout,
+            batch_first=True,
+        )
+
+        self.final_dropout = nn.Dropout(dropout)
+
+        self.decoder = nn.Linear(hidden_size, vocab_size)
+
+
+
+    def forward(self, x_ids, state=None):
+        one_hot = F.one_hot(x_ids, num_classes=self.vocab_size).float()
+
+        if state is not None:
+            outputs, new_state = self.lstm(one_hot, state)
+        else:
+            outputs, new_state = self.lstm(one_hot)
+
+        outputs = self.final_dropout(outputs)
+
+        logits = self.decoder(outputs)
+
+        return logits, new_state
+
--- train_rnn.py
+++ train_rnn.py
@@ -5,46 +5,10 @@ import click
 from tqdm import tqdm
 
 import torch
-import torch.nn as nn
 import torch.nn.functional as F
 
+from karpathy_lstm import KarpathyLSTM
 from next_byte_dataset import NextByteDataset, batchify, read_corpus_bytes
-
-
-class KarpathyLSTM(nn.Module):
-
-    def __init__(self, vocab_size, hidden_size, num_layers, dropout):
-        super().__init__()
-
-        self.vocab_size = vocab_size
-
-        self.lstm = nn.LSTM(
-            input_size=vocab_size,
-            hidden_size=hidden_size,
-            num_layers=num_layers,
-            dropout=dropout,
-            batch_first=True,
-        )
-
-        self.final_dropout = nn.Dropout(dropout)
-
-        self.decoder = nn.Linear(hidden_size, vocab_size)
-
-
-
-    def forward(self, x_ids, state=None):
-        one_hot = F.one_hot(x_ids, num_classes=self.vocab_size).float()
-
-        if state is not None:
-            outputs, new_state = self.lstm(one_hot, state)
-        else:
-            outputs, new_state = self.lstm(one_hot)
-
-        outputs = self.final_dropout(outputs)
-
-        logits = self.decoder(outputs)
-
-        return logits, new_state
 
 
 def generate_sample_text(model, vocab, id_to_byte, byte_to_id, length, temperature=0):
