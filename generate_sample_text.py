7af829de192d42099d665f468322561a60bc9b95
Giles Thomas <giles@giles.net>
2025-09-19 00:58:03 +0100

    Allow a primer for the sample text generation

--- generate_sample_text.py
+++ generate_sample_text.py
@@ -18,19 +18,24 @@ def sample(logits, temperature):
     return next_id
 
 
-def generate_sample_text(model, tokenizer, length, temperature=0):
+def generate_sample_text(model, tokenizer, length, primer_text=None, temperature=0):
     assert length >= 2
 
     with torch.no_grad():
         model.eval()
         hidden_state = None
-        primer = tokenizer.encode([tokenizer.random_vocab_byte()]).unsqueeze(0)
+        if primer_text is None:
+            primer_bytes = [tokenizer.random_vocab_byte()]
+        else:
+            primer_bytes = primer_text.encode("utf-8")
+        primer = tokenizer.encode(primer_bytes).unsqueeze(0)
         primer = primer.to(next(model.parameters()).device)
+        primer_len = primer.shape[-1]
 
         y_logits, hidden_state = model(primer)
         next_id = sample(y_logits, temperature)
-        output_ids = [primer.item(), next_id.item()]
-        for ii in range(length - 2):
+        output_ids = primer.flatten().tolist() + [next_id.item()]
+        for ii in range(length - 1 - primer_len):
             y_logits, hidden_state = model(next_id, hidden_state)
             next_id = sample(y_logits, temperature)
             output_ids.append(next_id.item())
@@ -52,7 +57,12 @@ def generate_sample_text(model, tokenizer, length, temperature=0):
     default=0.0, show_default=True,
     help="Sampling temperature (0 = greedy)"
 )
-def main(directory, run_name, checkpoint, length, temperature):
+@click.option(
+    "--primer_text", "-p", type=str,
+    default=None, show_default=True,
+    help="Primer text to start with.  If not defined, a random vocab byte is used"
+)
+def main(directory, run_name, checkpoint, length, temperature, primer_text):
     run = RunData(directory, run_name)
 
     model, tokenizer = load_checkpoint(run, checkpoint)
@@ -61,7 +71,8 @@ def main(directory, run_name, checkpoint, length, temperature):
 
     text = generate_sample_text(
         model, tokenizer,
-        length=length, temperature=temperature
+        length=length, primer_text=primer_text,
+        temperature=temperature,
     )
     print(text.decode("utf-8", errors="replace"))
 
