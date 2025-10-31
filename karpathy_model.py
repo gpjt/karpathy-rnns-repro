6105e6c08b05a4ec88671c43709eac69b6140a24
Giles Thomas <giles@giles.net>
2025-10-31 19:57:13 +0000

    Make the internal model configurable on KarpathyModel

--- karpathy_model.py
+++ karpathy_model.py
@@ -4,12 +4,15 @@ import torch.nn.functional as F
 
 class KarpathyModel(nn.Module):
 
-    def __init__(self, vocab_size, hidden_size, num_layers, dropout):
+    def __init__(
+        self, vocab_size, hidden_size, num_layers, dropout,
+        internal_model_class=nn.LSTM
+    ):
         super().__init__()
 
         self.vocab_size = vocab_size
 
-        self.lstm = nn.LSTM(
+        self.internal_model = internal_model_class(
             input_size=vocab_size,
             hidden_size=hidden_size,
             num_layers=num_layers,
@@ -22,14 +25,13 @@ class KarpathyModel(nn.Module):
         self.decoder = nn.Linear(hidden_size, vocab_size)
 
 
-
     def forward(self, x_ids, state=None):
         one_hot = F.one_hot(x_ids, num_classes=self.vocab_size).float()
 
         if state is not None:
-            outputs, new_state = self.lstm(one_hot, state)
+            outputs, new_state = self.internal_model(one_hot, state)
         else:
-            outputs, new_state = self.lstm(one_hot)
+            outputs, new_state = self.internal_model(one_hot)
 
         outputs = self.final_dropout(outputs)
 
